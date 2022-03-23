8ee0970eefbabcec23a595de78eb99cf6ee6a1eb
tapia1013 <manbearpig1013@gmail.com>
2022-03-23 10:23:15 -0700

    installed bcryptjs and hashed password in users

---
+++ backend/data/users.js
@@ -0,0 +1,24 @@
+import bcrypt from 'bcryptjs';
+
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    // hash password with bcryptjs.hashsync()
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true
+  },
+  {
+    name: 'Cookie',
+    email: 'cookie@example.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true
+  },
+  {
+    name: 'Nye',
+    email: 'nye@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+];
+
+export default users;
