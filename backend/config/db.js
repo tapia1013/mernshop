3be49009ee7819144b4190dfadaf79deef19abb2
tapia1013 <manbearpig1013@gmail.com>
2022-03-23 07:37:33 -0700

    connecteed app to the database

---
+++ backend/config/db.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+// When we deal with mongo we always get a promise
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    })
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`)
+    process.exit(1)
+  }
+}
+
+export default connectDB;
+
+
