51599a8d74b453e25f920f879134b7e8e09d028d
tapia1013 <manbearpig1013@gmail.com>
2022-03-23 20:05:53 -0700

    user register and password encryption

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -24,12 +24,23 @@ const userSchema = mongoose.Schema({
   timestamps: true
 });
 
+
 // method to match hashpasswords and given password
 userSchema.methods.matchPassword = async function (enteredPassword) {
   // bcrypt has a built in method to compare
   return await bcrypt.compare(enteredPassword, this.password)
 }
 
+// before we save we encrypt
+userSchema.pre('save', async function (next) {
+  // if password hasnt been modified next();
+  if (!this.isModified('password')) {
+    next()
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
 
 const User = mongoose.model('User', userSchema);
 
